894bef8c9ff1bd0e7eb987340f4dc13f2453f7b4
dominiofast <92938911+dominiofast@users.noreply.github.com>
2025-06-15 22:55:31 -0400

    frot

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -22,5 +25,6 @@ export default defineConfig({
   build: {
     outDir: 'build',
     sourcemap: true,
+    emptyOutDir: true,
   },
 }); 
